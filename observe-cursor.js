42fa40d1c4252a83f72fc7bf904a31753f9f9be7
darkomijic <darko.mijic@gmail.com>
2016-08-18 13:07:58 +0200

    Clear observable cursor using dedicated mobx method

--- observe-cursor.js
+++ observe-cursor.js
@@ -1,13 +1,13 @@
 import { Tracker } from 'meteor/tracker';
 import { action } from 'mobx';
 
-export default (actionPrefix = '', storeAttribute, handle, cursor) => {
+export default (actionPrefix = '', observableArray, handle, cursor) => {
 
   if (handle.ready()) {
     // initial fetch...
     Tracker.nonreactive(() => {
       action(`${actionPrefix}: initial fetch`, (comments) => {
-        storeAttribute.replace(comments);
+        observableArray.replace(comments);
       })(cursor.fetch());
     });
 
@@ -17,22 +17,22 @@ export default (actionPrefix = '', storeAttribute, handle, cursor) => {
       // just fetch them once (see above)
       _suppress_initial: true,
       addedAt: action(`${actionPrefix}: document added`, (document, atIndex) => {
-        storeAttribute.splice(atIndex, 0, document);
+        observableArray.splice(atIndex, 0, document);
       }),
       changedAt: action(`${actionPrefix}: document changed`, (newDocument, oldDocument, atIndex) => {
-        storeAttribute.splice(atIndex, 1, newDocument);
+        observableArray.splice(atIndex, 1, newDocument);
       }),
       removedAt: action(`${actionPrefix}: document removed`, (oldDocument, atIndex) => {
-        storeAttribute.splice(atIndex, 1);
+        observableArray.splice(atIndex, 1);
       }),
       movedTo: action(`${actionPrefix}: document moved`, (document, fromIndex, toIndex) => {
-        storeAttribute.splice(fromIndex, 1);
-        storeAttribute.splice(toIndex, 0, document);
+        observableArray.splice(fromIndex, 1);
+        observableArray.splice(toIndex, 0, document);
       }),
     });
   } else {
     action(`${actionPrefix}: initialized`, () => {
-      storeAttribute.replace([]);
+      observableArray.clear();
     })();
   }
 };
